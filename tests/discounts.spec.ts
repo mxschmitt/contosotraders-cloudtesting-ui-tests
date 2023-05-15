170aa1ea4f8ac05ff89e89d0b82343c53d8bc820
Max Schmitt <max@schmitt.mx>
2023-05-15 09:28:39 +0200

    chore: simplify first test

--- tests/discounts.spec.ts
+++ tests/discounts.spec.ts
@@ -1,32 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-const productId = 1;
-const validDiscountCodes = ['DISCOUNT15', 'DISCOUNT10'];
-const invalidDiscountCodes = ['DISCOUNT20', 'DISCOUNT30'];
-
-// SETUP: Add product to cart, view cart
-test.beforeEach(async ({ page }) => {
-    await page.goto(`/product/detail/${productId}`);
+test(`should be able to use discount code DISCOUNT15`, async ({ page }) => {
+    await page.goto(`/product/detail/1`);
     await page.getByRole('button', { name: 'Add To Bag' }).click();
     await page.getByRole('button', { name: 'cart' }).click();
-})
 
-test.describe('Discount Codes', () => {
-    for (const code of validDiscountCodes) {
-        test(`should be able to use VALID discount code ${code}`, async ({ page }) => {
-            await page.getByPlaceholder('Enter coupon code').fill(code);
-            await page.getByRole('button', { name: 'CHECK' }).click();
-            await expect(page.getByRole('button', { name: code })).toBeVisible();
-            // check that correct discount is applied
-            await expect(page.getByTestId('discount')).toHaveText(`-$${code.replace('DISCOUNT', '')}.00`);
-        });
-    }
-
-    for (const code of invalidDiscountCodes) {
-        test(`should not be able to use INVALID discount code ${code}`, async ({ page }) => {
-            await page.getByPlaceholder('Enter coupon code').fill(code);
-            await page.getByRole('button', { name: 'CHECK' }).click();
-            await expect(page.getByText('This coupon is invalid')).toBeVisible();
-        });
-    }
+    const code = 'DISCOUNT15';
+    const discountInPercentage = code.replace('DISCOUNT', '');
+    
+    await page.getByPlaceholder('Enter coupon code').fill(code);
+    await page.getByRole('button', { name: 'CHECK' }).click();
+    await expect(page.getByRole('button', { name: code })).toBeVisible();
+    // check that correct discount is applied
+    await expect(page.getByTestId('discount')).toHaveText(`-$${discountInPercentage}.00`);
 });
